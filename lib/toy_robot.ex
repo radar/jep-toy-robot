2323e07366ed32d07b0cc4545b8dce59616e659a
Jake Pitman <jakeseanp@gmail.com>
2019-06-11 16:46:54 +1000

    add east and south movement options

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -11,10 +11,22 @@ defmodule ToyRobot do
       iex> ToyRobot.move(%{north: 0, east: 0, dir: "NORTH"})
       %{north: 1, east: 0, dir: "NORTH"}
 
-      iex> %{north: 0, east: 0, dir: "NORTH"} |> ToyRobot.move |> ToyRobot.move
-      %{north: 2, east: 0, dir: "NORTH"}
+      iex> %{north: 0, east: 0, dir: "EAST"} |> ToyRobot.move |> ToyRobot.move
+      %{north: 0, east: 2, dir: "EAST"}
   """
-  def move(%{north: north, east: east, dir: "NORTH"}) do
-    %{north: north + 1, east: east, dir: "NORTH"}
+  def move(%{north: north, east: east, dir: "NORTH" = dir}) do
+    %{north: north + 1, east: east, dir: dir}
+  end
+
+  def move(%{north: north, east: east, dir: "EAST" = dir}) do
+    %{north: north, east: east + 1, dir: dir}
+  end
+
+  def move(%{north: north, east: east, dir: "WEST" = dir}) do
+    %{north: north, east: east - 1, dir: dir}
+  end
+
+  def move(%{north: north, east: east, dir: "SOUTH" = dir}) do
+    %{north: north - 1, east: east, dir: dir}
   end
 end
--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -2,7 +2,11 @@ defmodule ToyRobotTest do
   use ExUnit.Case
   doctest ToyRobot
 
-  test "more complex stuff goes here" do
-
+  test "moves west when dir == 'WEST'" do
+    assert ToyRobot.move(%{north: 0, east: 3, dir: "WEST"}) == %{north: 0, east: 2, dir: "WEST"}
   end
+  test "moves south when dir == 'SOUTH'" do
+    assert ToyRobot.move(%{north: 3, east: 0, dir: "SOUTH"}) == %{north: 2, east: 0, dir: "SOUTH"}
+  end
+
 end
