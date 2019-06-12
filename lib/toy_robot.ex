e409056bafe6d237da94aa16d90a37ab89e6bca5
Jake Pitman <jakeseanp@gmail.com>
2019-06-12 15:19:30 +1000

    update tests

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -7,6 +7,15 @@ defmodule ToyRobot do
   Documentation for ToyRobot.
   """
 
+  @doc """
+  checks if robot is facing boundary, returns :ok if robot can move forward, :error if can't
+  ## Examples
+      iex> ToyRobot.check_boundary(%{north: 0, east: 0, dir: "NORTH"})
+      {:ok, %{north: 0, east: 0, dir: "NORTH"}}
+
+      iex> ToyRobot.check_boundary(%{north: 0, east: 0, dir: "WEST"})
+      {:error, "Facing boundary, cannot move", %{north: 0, east: 0, dir: "WEST"}}
+  """
   def check_boundary(%{north: 4, east: _, dir: "NORTH"} = position) do
     {:error, "Facing boundary, cannot move", position}
   end
@@ -27,22 +36,20 @@ defmodule ToyRobot do
     {:ok, position}
   end
   
+
+  @doc """
+  Moves one space forward & returns new pos if check_boundary passes. Returns current pos if check_boundary fails
+  ## Examples
+      iex> ToyRobot.move(%{north: 0, east: 0, dir: "NORTH"})
+      %{north: 1, east: 0, dir: "NORTH"}
+
+      iex> %{north: 0, east: 4, dir: "EAST"} |> ToyRobot.move
+      %{north: 0, east: 4, dir: "EAST"}
+  """
   def move(%{north: _, east: _, dir: _} = position) do
     check_boundary(position)
     |> move()
   end
-
-  @doc """
-  Moves one space forward.
-
-  ## Examples
-
-      iex> ToyRobot.move(%{north: 0, east: 0, dir: "NORTH"})
-      %{north: 1, east: 0, dir: "NORTH"}
-
-      iex> %{north: 0, east: 0, dir: "EAST"} |> ToyRobot.move |> ToyRobot.move
-      %{north: 0, east: 2, dir: "EAST"}
-  """
 
   def move({:ok, %{north: north, east: east, dir: "NORTH" = dir}}) do
     %{north: north + 1, east: east, dir: dir}
--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -2,13 +2,15 @@ defmodule ToyRobotTest do
   use ExUnit.Case
   doctest ToyRobot
 
-  test "#move moves west when dir == 'WEST'" do
-    assert ToyRobot.move(%{north: 0, east: 3, dir: "WEST"}) == %{north: 0, east: 2, dir: "WEST"}
+  describe "move" do
+    test "#move moves west when dir == 'WEST'" do
+      assert ToyRobot.move(%{north: 0, east: 3, dir: "WEST"}) == %{north: 0, east: 2, dir: "WEST"}
+    end
+    test "#move moves south when dir == 'SOUTH'" do
+      assert ToyRobot.move(%{north: 3, east: 0, dir: "SOUTH"}) == %{north: 2, east: 0, dir: "SOUTH"}
+    end
   end
 
-  test "#move moves south when dir == 'SOUTH'" do
-    assert ToyRobot.move(%{north: 3, east: 0, dir: "SOUTH"}) == %{north: 2, east: 0, dir: "SOUTH"}
-  end
 
   test "#check_boundary returns {:ok, position} when not facing boundary" do
     position = %{north: 0, east: 0, dir: "NORTH"}
