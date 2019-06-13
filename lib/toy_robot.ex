0822767b009736b5ea7035e1bbf30455adf39b8d
Jake Pitman <jakeseanp@gmail.com>
2019-06-13 14:09:32 +1000

    add place/1 + place/2 functions

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -105,4 +105,56 @@ defmodule ToyRobot do
   end
 
 
+  @doc """
+  Places the position to a new set of coordinates if within boundaries
+  Places position at 0,0,NORTH if given invalid coordinates
+  ## Examples
+      iex> ToyRobot.place("PLACE,3,3,NORTH")
+      %ToyRobot{north: 3, east: 3, dir: "NORTH"}
+
+      iex> ToyRobot.place("PLACE,3,8,NORTH")
+      %ToyRobot{north: 0, east: 0, dir: "NORTH"}
+  """
+
+  def place(string_command) do
+    coord = string_command_to_map(string_command)
+    boundary_range = 0..4
+    if Enum.member?(boundary_range, coord[:east]) && Enum.member?(boundary_range, coord[:north]) do
+      %ToyRobot{ north: coord[:north], east: coord[:east], dir: coord[:dir] }
+    else
+      IO.puts("Invalid PLACE coordinates" )
+      IO.puts("No previous position to revert to" )
+      IO.puts("Placing robot at 0,0,NORTH")
+      %ToyRobot{north: 0, east: 0, dir: "NORTH"}
+    end
+  end
+
+  @doc """
+  Places the position to a new set of coordinates
+  Reverts back to prev. position when given invalid coordinates
+  ## Examples
+    iex> ToyRobot.place("PLACE,3,3,NORTH", %ToyRobot{north: 1, east: 1, dir: "EAST"})
+    %ToyRobot{north: 3, east: 3, dir: "NORTH"}
+  """
+
+  def place(string_command, %ToyRobot{} = previous_position) do
+    coord = string_command_to_map(string_command)
+    boundary_range = 0..4
+    if Enum.member?(boundary_range, coord[:east]) && Enum.member?(boundary_range, coord[:north]) do
+      %ToyRobot{ north: coord[:north], east: coord[:east], dir: coord[:dir] }
+    else
+      IO.puts("Invalid PLACE coordinates" )
+      IO.puts("Reverting to previous position" )
+      previous_position
+    end
+  end
+
+  defp string_command_to_map(string_command) do
+    split_string = String.split(string_command, ",")
+    east = Enum.at(split_string, 1) |> String.to_integer
+    north = Enum.at(split_string, 2) |> String.to_integer
+    dir = Enum.at(split_string, 3)
+    %{east: east, north: north, dir: dir}
+  end
+
 end
